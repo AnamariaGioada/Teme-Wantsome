e8e679201f5e2cb9cfea87853628eb3d6c4d3822
AnamariaGioada <anamaria.damaschin@gmail.com>
2024-07-01 22:26:44 +0300

    Rezolvare Homework JS Events shopping list

--- Shopping-List/shopping-list.js
+++ Shopping-List/shopping-list.js
@@ -1,10 +1,56 @@
-const btn = document.getElementById('submitButton');
-const txt = document.getElementById('addedText');
-let ul = document.getElementById('ul')
+const addForm = document.getElementById("shoppingList");
+const txt = document.getElementById("addedText");
+const submitButton = document.getElementById("submitButton");
+let ul = document.getElementById("ul");
 
-btn.addEventListener('click', function() {
-    let list = document.createElement('li');
-    list.innerHTML = txt.value;
-    ul.appendChild(list);
+addForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if (txt.value === "") {
+    alert("Please add item");
+    return;
+  }
+  if (ul.children.length > 9) {
+    submitButton.disabled = true;
+    return;
+  }
 
+  let list = document.createElement("li");
+
+  const item = document.createElement("span");
+
+  let checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+
+  checkbox.addEventListener("change", function (e) {
+    if (checkbox.checked) {
+      item.style.textDecoration = "line-through";
+      list.style.backgroundColor = "lightyellow";
+    } else if (!checkbox.checked) {
+      item.style.textDecoration = "none";
+      list.style.backgroundColor = "unset";
+    }
+  });
+
+  const spanText = document.createElement("span");
+  spanText.innerText = txt.value;
+
+  item.appendChild(checkbox);
+
+  item.appendChild(spanText);
+  list.appendChild(item);
+
+  let deleteButton = document.createElement("button");
+  deleteButton.className = "fa fa-trash delete";
+  deleteButton.type = "button";
+
+  deleteButton.addEventListener("click", function (e) {
+    ul.removeChild(list);
+    submitButton.disabled = false;
+  });
+  list.appendChild(deleteButton);
+
+  ul.appendChild(list);
+
+  txt.value = "";
+  txt.focus();
 });
